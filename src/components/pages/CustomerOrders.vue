ecc61c800d756311ab372c2d2aa223ca97dc76bb
yclin1815 <yclin1815@gmail.com>
2021-03-23 15:01:45 +0800

    刪除購物車品項及優惠碼功能

--- src/components/pages/CustomerOrders.vue
+++ src/components/pages/CustomerOrders.vue
@@ -135,18 +135,20 @@
             <th>數量</th>
             <th>單價</th>
           </thead>
-          <tbody>
-            <tr v-for="item in cart.carts" :key="item.id" v-if="cart.carts">
+          <tbody v-if="cart.carts">
+            <tr v-for="item in cart.carts" :key="item.id">
               <td class="align-middle">
-                <button type="button" class="btn btn-outline-danger btn-sm">
+                <button
+                  type="button"
+                  class="btn btn-outline-danger btn-sm"
+                  @click="removeCartItem(item.id)"
+                >
                   <i class="far fa-trash-alt"></i>
                 </button>
               </td>
               <td class="align-middle">
                 {{ item.product.title }}
-                <!-- <div class="text-success" v-if="item.coupon">
-                  已套用優惠券
-                </div> -->
+                <div class="text-success" v-if="item.coupon">已套用優惠券</div>
               </td>
               <td class="align-middle">
                 {{ item.qty }}/{{ item.product.unit }}
@@ -159,12 +161,29 @@
               <td colspan="3" class="text-right">總計</td>
               <td class="text-right">{{ cart.total }}</td>
             </tr>
-            <!-- <tr v-if="cart.final_total">
+            <tr v-if="cart.final_total !== cart.total">
               <td colspan="3" class="text-right text-success">折扣價</td>
               <td class="text-right text-success">{{ cart.final_total }}</td>
-            </tr> -->
+            </tr>
           </tfoot>
         </table>
+        <div class="input-group mb-3 input-group-sm">
+          <input
+            type="text"
+            class="form-control"
+            v-model="coupon_code"
+            placeholder="請輸入優惠碼"
+          />
+          <div class="input-group-append">
+            <button
+              class="btn btn-outline-secondary"
+              type="button"
+              @click="addCouponCode"
+            >
+              套用優惠碼
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -183,6 +202,7 @@ export default {
       },
       cart: {},
       isLoading: false,
+      coupon_code: "",
     };
   },
   methods: {
@@ -229,6 +249,28 @@ export default {
       this.$http.get(url).then((response) => {
         vm.cart = response.data.data;
         console.log(response);
+        vm.isLoading = false;
+      });
+    },
+    removeCartItem(id) {
+      const vm = this;
+      const url = `${process.env.VUE_APP_API}/api/${process.env.VUE_APP_CUSTOMPATH}/cart/${id}`;
+      vm.isLoading = true;
+      this.$http.delete(url).then(() => {
+        vm.getCart();
+        vm.isLoading = false;
+      });
+    },
+    addCouponCode() {
+      const vm = this;
+      const url = `${process.env.VUE_APP_API}/api/${process.env.VUE_APP_CUSTOMPATH}/coupon`;
+      const coupon = {
+        code: vm.coupon_code,
+      };
+      vm.isLoading = true;
+      this.$http.post(url, { data: coupon }).then((response) => {
+        console.log(response);
+        vm.getCart();
         vm.isLoading = false;
       });
     },
